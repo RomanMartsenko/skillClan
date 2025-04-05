0dbe7efb87e1473375128ae67a1abb1ea8bc7190
Martsenko Roman <roman.martsenko@kyivstar.net>
2025-04-05 23:25:06 +0300

    upd config

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
-import 'dotenv/config'; // дозволяє читати .env
-
 export default {
   server: {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,  // Налаштування порту для вашого сервера
   },
   database: {
     host: process.env.PGHOST,
-    port: process.env.PGPORT,
+    port: process.env.PGPORT || 5432,
     database: process.env.PGDATABASE,
     user: process.env.PGUSER,
-    password: process.env.PGPASSWORD
+    password: process.env.PGPASSWORD,
   }
 };
